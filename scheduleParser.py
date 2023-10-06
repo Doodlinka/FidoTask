5dd57fa9c0857496a58e4b12f47f4eb143f989dc
Doodlinka <oleshko.darya@gmail.com>
2023-10-06 14:34:10 +0300

    notes

---
+++ scheduleParser.py
@@ -0,0 +1,10 @@
+# input: .tsv or .doc
+# output: a .json with all of the specialties
+
+# json structure:
+# faculty
+# year
+# specialty
+# ??? (but putthe teacher separately if you can)
+
+# TODO: write down marker constants (faculties, specialties, week days)
